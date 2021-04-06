65de19006e434ffe5258c4899716402384df3c17
Ian <pellet.ian1@gmail.com>
2021-04-06 09:51:17 +0200

    automatic change 0/1 to 330/250 for mesh size

--- Marine/Programmes/load_MPs_data.m
+++ Marine/Programmes/load_MPs_data.m
@@ -3,9 +3,13 @@ function [T] = load_MPs_data(data_file)
 %   Loads a .txt file and sets the variable type of the columns.
     opts = detectImportOptions(data_file);
     opts = setvartype(opts,{'station'},'string');
-    opts = setvartype(opts,{'replica', 'type', 'n'},'uint32');
+    opts = setvartype(opts,{'replica', 'mesh_size', 'type', 'n'},'uint32');
     opts = setvartype(opts,{'date', 'time'},'datetime');
     disp([opts.VariableNames' opts.VariableTypes']);
     T = readtable(data_file, opts);
+    % {0 : 330, 1 : 250}
+    T(T(:,"mesh_size").Variables==0,"mesh_size") = array2table(ones(size(T(T(:,"mesh_size").Variables==0,"mesh_size")))*330);
+    T(T(:,"mesh_size").Variables==1,"mesh_size") = array2table(ones(size(T(T(:,"mesh_size").Variables==1,"mesh_size")))*250);
+    
 end
 
