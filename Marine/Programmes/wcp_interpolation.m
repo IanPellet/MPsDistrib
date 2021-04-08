125628c47d31730aa5c9ec6fd44c23a1a0cff1ee
Ian <pellet.ian1@gmail.com>
2021-04-08 10:31:11 +0200

    fixed moving avg

--- Marine/Programmes/wcp_interpolation.m
+++ Marine/Programmes/wcp_interpolation.m
@@ -6,17 +6,17 @@ function [Kz, dKz] = wcp_interpolation(z_mes, Kz_mes, z)
 %% Linear interpolation 
 Kz = interp1(z_mes, Kz_mes, z,'linear');
 
-disp(size(Kz))
+%disp(size(Kz))
 
 %% 8-point moving average
 npts = 8;
-wts = ones(1,npts); % convolution matrix
+wts = ones(1,npts)*1/npts; % convolution matrix
 Kz = [zeros(1,npts/2) Kz zeros(1,npts/2+1)]; % padding
 Kz = conv(Kz,wts,'valid');
-disp(size(Kz))
+%disp(size(Kz))
 
 dKz = diff(Kz);
-disp(size(dKz))
+%disp(size(dKz))
 %dKz = conv(dKz,wts,'valid');
 
 end
