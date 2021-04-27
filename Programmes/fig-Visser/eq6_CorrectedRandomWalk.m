66b4b749fb1c21dfe66f4cc1899ff193a4a66248
Ian <pellet.ian1@gmail.com>
2021-04-27 15:23:10 +0200

    correctedRandomWalk - uniform except 1st mesh

---
+++ Programmes/fig-Visser/eq6_CorrectedRandomWalk.m
@@ -0,0 +1,14 @@
+function [znew] = eq6_CorrectedRandomWalk(zi,~,dKzi,K,dt,dz)
+%EQ6_CORRECTEDRANDOMWALK Diffusivity model
+%   
+
+    pd = makedist('Uniform', 'lower', -1, 'upper', 1);
+    r = 1/3;
+    R = random(pd,size(zi));
+    ziOffset = zi + dKzi*dt/2;
+    index = max(1, fix(ziOffset/dz));
+    KziOffset = K(index);
+    znew = zi + dKzi*dt + R.*sqrt(2/r*KziOffset*dt);
+    
+end
+
