3da212c1c83c1d43e7e18e1618949c8132327fa2
Ian <pellet.ian1@gmail.com>
2021-05-04 16:36:27 +0200

    Changed to run EstimationRhoP

--- Programmes/fig-Visser/Diffusivity.m
+++ Programmes/fig-Visser/Diffusivity.m
@@ -1,10 +1,15 @@
-function [K,dK] = Diffusivity(z,z_,dz)
+function [K,dK] = Diffusivity(z,z_,dz,tension,cutoff,KZ_Fev10,z0)
 %DIFFUSIVITY Returns diffusivity profile
 %   Pt Somlit, 10 Fev
 
-addpath('../');
+if nargin == 5
+    addpath('../');
+    DensiteFevrierRhoma
+end
 
-DensiteFevrierRhoma 
+
+z0 = [-z(end) ; z0];
+KZ_Fev10 = [0 KZ_Fev10];
 
 Kinter = interp1(z0, KZ_Fev10, -z,'linear');
 
@@ -14,13 +19,15 @@ wts = ones(1,npts)*1/npts; % convolution matrix
 Kmavg = conv(Kinter,wts,'valid');
 zmavg = conv(z,wts,'valid');
 
-tens = 0.5;
-cutoff = 0.1;
-Kspl = spline1d (z, zmavg, Kmavg, [min(z) max(z)], [0 0], tens, cutoff)';
-K = spline1d (z_, zmavg, Kmavg, [min(z_) max(z_)], [0 0], tens, cutoff)';
+% tension = 0.5;
+% cutoff = 0.1;
+Kspl = spline1d (z, zmavg, Kmavg, [min(z) max(z)], [0 0], tension, cutoff)';
+K = spline1d (z_, zmavg, Kmavg, [min(z_) max(z_)], [0 0], tension, cutoff)';
+Kspl(Kspl<0) = 0;
+K(K<0) = 0;
 
 dK = diff(Kspl)/dz;
-
+% 
 % figure(1), clf
 % plot(K,-z_)
 % figure(2), clf
