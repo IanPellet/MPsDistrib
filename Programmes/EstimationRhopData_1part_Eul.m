556df83e3fb76d2e023cdeeb6348656ce5493cb6
Ian <pellet.ian1@gmail.com>
2021-05-12 10:37:05 +0200

    commented

--- Programmes/EstimationRhopData_1part_Eul.m
+++ Programmes/EstimationRhopData_1part_Eul.m
@@ -1,43 +1,42 @@
-function [Resultats,ConcentrationSample,Ztest_,z_] = EstimationRhopData_1part_Eul(SizePart, RhoP_test, saveFig)
+function [Resultats,ConcentrationSample,Ztest_,z_] = EstimationRhopData_1part_Eul(SizePart, RhoP_test, saveFig, N)
 %%ESTIMATIONRHOPDATA_1PART_EUL Finds the modeled particle density closest
 %%to data
 % SizePart : (double) size of the modeled particles (m)
 % RhoP_tes : (double array) modeled particle densities tested (kg.m⁻³)
 % saveFig : (boolean) if true figures will be saved to '../Results/EstimRho/'
+% N : (int) default = 500, number of meshes. High : +precise -fast / Low : -precise +fast
 % 
-% 
+% example : EstimationRhopData_1part_Eul(400e-6, 900:1100, true, 200)
 
 %% Set Parameters
 path = '../Results/EstimRho/'; % saved figures directory
 
-% Wind parameters
-day = '10fev';
+%% Water column parameters
+L = 51; % depth (m)
+if nargin < 4
+    N = 500; % number of meshes. High : +precise -fast / Low : -precise +fast
+end
+dz= L/N; % size of meshes
+z = (0:dz:L)'; % meshes boundaries
+z_ = z(1:end-1)+dz/2; % center of the meshes
 
+day = '10fev'; % day corresponding to diffusive turbulence data
 
-%% Data loading
-L = 51;
-N = 100;
-dz= L/N;
-z = (0:dz:L)';
-z_ = z(1:end-1)+dz/2;
-
+%% Data
 % SamplingDate = datetime('3/18/2021');
 % DataFile = '../Data/data_mps.txt';
 % [ConcentrationSample, DepthSample] = getDataNpart(false, SizePart, true, DataFile, SamplingDate);
-CMes=[0.62 0.34 0.06 0.02 0];
-ZMes=[1 10 15 40 50];
-% ConcentrationSample = interp1(ZMes,CMes,z_,'pchip')';
-% DepthSample = z_';
+CMes=[0.62 0.34 0.06 0.02 0]; % Concentration
+ZMes=[1 10 15 40 50]; % Depth of the samples
 ConcentrationSample = CMes';
 DepthSample = ZMes';
-DataInterp = interp1(ZMes,CMes,z_,'pchip')';
-
+DataInterp = interp1(ZMes,CMes,z_,'pchip')'; % Interpolated data
 
 dh = 0.71; % Net oppening (m)
 
-% Boundaries in between which modeled part number have to be tested
-boundTest = zeros(length(DepthSample)*2,1);
-ibound = zeros(size(boundTest));
+%% Find corresponding depths to get from the model
+boundTest = zeros(length(DepthSample)*2,1); % Boundaries in between which modeled part number have to be tested
+ibound = zeros(size(boundTest)); % index boundaries of meshes to avg
 
 for i = 1:length(boundTest)
     if mod(i,2)
@@ -48,11 +47,11 @@ for i = 1:length(boundTest)
     ibound(i) = min(length(z_),max(1,fix(boundTest(i)/dz)+1));
 end, clear i,
 
+Ztest_ = DepthSample + dh/2; 
 
-Ztest_ = DepthSample + dh/2;
-
+%% Simulations
 clear Resultats
-
+% Initalisation of results data structure fields
 Resultats(1:length(RhoP_test)) = struct('RhoP', 0, 'ConcentrationModel', zeros(size(ConcentrationSample)),...
     'Alpha', 0, 'Erreur', zeros(size(ConcentrationSample)), 'rmseErreur', 0);
 
@@ -62,23 +61,29 @@ else
     modSize = 350e-6;
 end
 
+% launch simulation for each rhop
 for iRes = 1:length(RhoP_test)
     RhoP = RhoP_test(iRes);    
+    
     [conc, z_] = Transport_Eulerian(modSize, RhoP, N, L, day);
     
+    % find the modeled concentration at depth corresponding with sample
     ConcentrationModel = zeros(size(ConcentrationSample));
     j = 0;
     for ic = 1:2:length(ibound)
         j = j+1;
         ConcentrationModel(j) = mean(conc(ibound(ic):ibound(ic+1)));
     end, clear ic j,
-
+    
+    % Compute the proportionnality coefficient minimizing the square error
+    % between model and data
     alpha = ConcentrationModel\ConcentrationSample;
     
-    
+    % Compute the root mean square error between model and data
     Erreur = abs(ConcentrationModel.*alpha - ConcentrationSample);
     rmseErreur = sqrt(mean(Erreur.^2,'omitnan'));
     
+    % Fill the result structure 
     Resultats(iRes).RhoP = RhoP;
     Resultats(iRes).ConcentrationModel = ConcentrationModel;
     Resultats(iRes).Alpha = alpha;
@@ -87,11 +92,14 @@ for iRes = 1:length(RhoP_test)
     Resultats(iRes).conc = conc';
 end, clear iRes,
 
-
-ttl = ['Particle size : ' num2str(modSize*1e6) 'µm'];
-minI = [Resultats.rmseErreur] == min([Resultats.rmseErreur]);
+% Find rhop correponding to minimal error
+minI = [Resultats.rmseErreur] == min([Resultats.rmseErreur]); 
 minRho = Resultats(minI).RhoP;
 
+%% Display results
+ttl = ['Particle size : ' num2str(modSize*1e6) 'µm']; % Figures titles
+
+% figure 1 : plot all simulation results
 f1 = figure(1); clf,
 plot(DataInterp,-z_,'--', 'DisplayName', 'Data interpolation');
 xlim([0 max(Resultats(minI).conc*Resultats(minI).Alpha)])
@@ -102,12 +110,12 @@ hold on
 plot(CMes,-ZMes,'pm','MarkerSize', 10, 'DisplayName', 'Sampled Data');
 for res = Resultats
     plot(res.conc*res.Alpha,-z_,'DisplayName', ['RhoP = ' num2str(res.RhoP) 'kg.m⁻³'])
-%     plot(res.ConcentrationModel*res.Alpha, -Ztest_,'*','DisplayName', ['RhoP = ' num2str(res.RhoP)])
 end
 legend('Location', 'southeast')
 title(ttl)
 hold off
 
+% figure 2 : plot error with respect to rhoP
 f2 = figure(2); clf,
 hold on
 plotErr = zeros(size([Resultats]));
@@ -120,6 +128,7 @@ ylabel('RMSE (mps.m⁻³)');
 title(ttl)
 hold off
 
+% figure 3 : plot best rhoP profile
 f3 = figure(3); clf,
 plot(DataInterp,-z_,'--', 'DisplayName', 'Data interpolation');
 xlim([0 max(Resultats(minI).conc*Resultats(minI).Alpha)])
@@ -133,6 +142,7 @@ legend('Location', 'southeast')
 title([ttl ' -- Rho_p = ' num2str(minRho) 'kg.m⁻³'])
 hold off
 
+%% Save figures
 if saveFig
     if length(RhoP_test) == 1
         rhoInter = num2str(RhoP_test);
