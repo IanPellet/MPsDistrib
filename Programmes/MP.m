1aed764ee8abe5a31cce5d2d0d289ffc6c65a0c9
Ian <pellet.ian1@gmail.com>
2021-05-18 17:35:50 +0200

    uncleaned MP transport

--- Programmes/MP.m
+++ Programmes/MP.m
@@ -1,72 +1,61 @@
 classdef MP
-    %MP Summary of this class goes here
+    %MP Micro-plastic class, represents one particle
     %   Detailed explanation goes here
     
     properties     
-        z_ % initial input, modified externaly
-%         Ks_ % external profile
-%         dKs_ % external profile
-        row_
+        z_ % particle's position (m)
+
+        dz_
     end
     
     properties (SetAccess = private)
-        type_ % input
-        size_ % input
+        type_ % int {0,1,2,3} NON IMPLÉMENTÉ
+        size_ % (m)
+        rho_ % (kg.m⁻³)
+        U_ % velocity in the water column, dep of type (m.s⁻¹)
     end
     
     properties (Dependent)
-        rho_ % dependent of type
-        U_ % dep of type
-        uz_ % dep of U and z
-%         Kz_ % dependent of z and Ks
-%         dKz_ % dependent of z and dKs
+        
+        uz_
+        index_
     end
     
     methods
-        function obj = MP(type,size, z_init, row)
+        function obj = MP(size, rhop, rhow)
+            %MP Constructor
+            % If no argument is passed (default constructor), everything
+            % is set to 0.
+            %
+            % PARAMETERS
+            % size : double, particle size (m)
+            % rhop : double, particle density (kg.m⁻³)
+            % z : double, particle's position (m)
+            % rhow : double, water density ((kg.m⁻³)
+            %
+            
+            obj.type_ = 0; % TYPE NOT IMPLEMENTED YET
             
             if nargin == 0 % default constructor
-                obj.type_ = 0;
                 obj.size_ = 0;
-                obj.z_ = 0;
-                obj.row_ = 0;
+                obj.rho_ = 0;
+%                 obj.z_ = 0;
+%                 obj.dz_ = 0;
+                obj.U_ = 0;
             else
-                obj.type_ = type;
                 obj.size_ = size;
-                obj.z_ = z_init;
-                obj.row_ = row;
+                obj.rho_ = rhop;
+%                 obj.z_ = z;
+%                 obj.dz_ = dz;
+                obj.U_ = obj.setU(rhow);
             end
-            %MP Construct an instance of this class
-            %   Size, type and initial position are given 
-            %obj.Ks_ = Ks;
-            %obj.dKs_ = dKs;
             
-%             get.rho_(obj);
-%             get.U_(obj);
-%             
-%             get.uz_(obj);
-            %eval_Kz(obj);
-            %eval_dKz(obj);
+            
+            
         end
         
-        
-        function rho = get.rho_(obj)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-%             load('rho_per_type', 'RHO_TYPE'); % loads a variable RHO_TYPE 
-            RHO_TYPE = [ 900 1050 ;
-                        1000 1300 ;
-                        1300 1400 ];
-            % RHO_TYPE = [rho_min1 rho_max1 ;
-            %             rho_min2 rho_max2 ;
-            %             rho_min3 rho_max3 ]
-            m = RHO_TYPE(obj.type_,1); % min Rho
-            M = RHO_TYPE(obj.type_,2); % max Rho
-            pd = makedist('Normal','mu',(m+M)/2 ,'sigma', (M-m)/4 ); % Normal law, centered on (m+M)/2, 95.45% of values between m and M
-            rho = random(pd);
-        end
-        
-        function U = get.U_(obj)
+
+        function value = setU(obj,rhow)
             %METHOD1 Summary of this method goes here
             %   Detailed explanation goes here
             Nom=[... 
@@ -75,32 +64,59 @@ classdef MP
                 ;{'Ahrens'}...%Ahrens (2000)
                ];
             indNom = 3;
-            S = obj.rho_./obj.row_; 
+            S = obj.rho_./rhow; 
             Ws = eval(['Vitesse' cell2mat(Nom(indNom)) '(obj.size_,S);']);
-            U = Ws; 
-            test = obj.rho_<obj.row_;
-            U(test) = -Ws(test);
+            value = Ws'; 
+            test = obj.rho_<rhow;
+            value(test) = -Ws(test);
         end
         
-        function uz = get.uz_(obj)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            
-            uz = obj.U_(index);
-        end
-        
-        
-%         function obj = eval_Kz(obj)
-%             %METHOD1 Summary of this method goes here
-%             %   Detailed explanation goes here
-%             obj.Kz_ = obj.Ks_(obj.z_);
+%         function uzVal = get.uz_(obj)
+%             uzVal = obj.U_(obj.index_);
 %         end
 %         
-%         function obj = eval_dKz(obj)
+%         function iVal = get.index_(obj)
+%             iVal = max(1, cast(obj.z_/obj.dz_, 'uint32'));
+%         end
+%         
+%         function new_z = Step_Lagrangien(obj, K, dK, dt, L)
+%             %STEP_LAGRANGIEN One step foward in time for the Lagrangian transport model
+%             %   The new value of x after dt is computed and returned by this function.
+%             
+%             Kz = K([obj.index_]);
+%             dKz = dK([obj.index_]);
+%             
+%             pd = makedist('Uniform', 'lower', -1, 'upper', 1);
+%             d = 1/3;
+%             R = random(pd,size(obj));
+% 
+%             new_z = obj.z_ + dt*obj.uz_ + dKz*dt + R.*(2/d*Kz*dt).^(1/2);
+%             % First-type boundary conditions
+%             % new_x = max(0,new_x);
+%             % new_x = min(L, new_x);
+% 
+%             % Reflective boundary conditions
+%             new_z(new_z < 0) = -new_z(new_z < 0);
+%             new_z(new_z > L) = L-new_z(new_z > L) + L;
+% 
+%             end
+        
+        %         function rho = get.rho_(obj)
 %             %METHOD1 Summary of this method goes here
 %             %   Detailed explanation goes here
-%             obj.dKz_ = obj.dKs_(obj.z_);
+% %             load('rho_per_type', 'RHO_TYPE'); % loads a variable RHO_TYPE 
+%             RHO_TYPE = [ 900 1050 ;
+%                         1000 1300 ;
+%                         1300 1400 ];
+%             % RHO_TYPE = [rho_min1 rho_max1 ;
+%             %             rho_min2 rho_max2 ;
+%             %             rho_min3 rho_max3 ]
+%             m = RHO_TYPE(obj.type_,1); % min Rho
+%             M = RHO_TYPE(obj.type_,2); % max Rho
+%             pd = makedist('Normal','mu',(m+M)/2 ,'sigma', (M-m)/4 ); % Normal law, centered on (m+M)/2, 95.45% of values between m and M
+%             rho = random(pd);
 %         end
+        
     end
 end
 
--- Programmes/MP_transport.m
+++ Programmes/MP_transport.m
@@ -1,46 +1,51 @@
-% 
 % function [T, MSEh] = MP_transport(dt, nPart, tf)
-% Lagrangian transportation model 
-% dt : time step
-% nPart : total number of particles in the model
-% tf : time of simulation
-% cKs : coefficient multipling Ks
 
-global L
-clear Concentration err
+clear
+
+
+
+%% Water column parameters
+L = 50;
+N = 50;
+dz= L/N;  z=0:dz:L; % z : boundaries of the meshes
+z_=(z(1:end-1)+z(2:end))/2; % middle of each mesh  
+
+%% Diffusivity
+DensiteFevrierRhoma
+KZ_day = KZ_Fev10;
+Row_day = Row_Fev10;
+z_day = z_Fev10;
+z__day = z__Fev10;
+addpath('./fig-Visser/');
+[K,dK] = Diffusivity(z,z_,dz,0.8,0,KZ_day,z_day);
+
+rhow = interp1(-z__day,Row_day,z,'pchip'); % density of sea water 
 
 %% Particules initialisation
-nPart = 100; % number of particles
+nPart = 5e3; % number of particles
 zPart = linspace(0, L, nPart); % depth of particles
-sizePart = linspace(250e-6, 5e-3, nPart); % size of particles
-typePart = fix(1 + (4-1).*rand(nPart)); % type of particles
+sizePart = linspace(250e-6, 5e-3, nPart); % size of particle
+rhop = 1000;
 
 % allocate memory to store particles
 clear part
-part(nPart) = MP(typePart(nPart),sizePart(nPart), zPart(nPart), row); % array of MP objects
+mp(nPart) = MP; % array of MP objects
 % Fill the array
-for i = 1:(nPart-1)
-    part(i) = MP(typePart(i),sizePart(i), zPart(i), row);
+for i = 1:(nPart)
+    mp(i) = MP(sizePart(i), rhop, rhow);
 end
 
-% Compute initial concentrations
-h_init = histogram([part.z_], "BinEdges", z, 'Visible', 'off').Values;
-C_init = h_init/dz;
-
-
 %% Simulation
-dt = 1; % Time step
-tf = 60*60*12; % Simulation time
+dt = 10; % Time step
+tf = 60*60; % Simulation time
 nStep = tf/dt+1; % Number of steps
 if cast(nStep, 'uint32') ~= nStep
     nStep = cast(nStep+1, 'uint32');
 end
 T = ones(nStep,1); % allocate memory to store time
-C_hist = ones(nStep,N); % allocate memory to store concentration
 
-z_past = [part.z_];
+z_past = zPart;
 step = 1;
-C_hist(step,:) = [C_init];
 
 t=0; OnContinue=true;
 T(step,1) = t;
@@ -51,28 +56,22 @@ while OnContinue
     T(step,1) = t;
     
     %% Particules update
-    index = max(1, fix([part.z_]/dz));
-    Upart = [part.U_];
-    [part.z_] = Step_Lagrangien([part.z_], Upart(index), [part.z_], [part.z_], dt);
-    index = max(1, cast(part(1,:)/dz, 'uint32'));
-    part(2,:) = u(index);
-    part(3,:) = K(index);
-    part(4,:) = dK(index);
+    index = max(1, cast(zPart/dz, 'uint32'));
+    U = [mp.U_];
+    uz = diag(U(index,:))';
+    zPart = Step_Lagrangien(zPart, uz, K(index), dK(index), dt, L);
     
     
     if true
 
-        z_present = part(1,:);
+        z_present = zPart;
         
         % Computation of the concentration of MPs in each mesh
         h_past = histogram(z_past, "BinEdges", z, 'Visible', 'off').Values;
         h_present = histogram(z_present, "BinEdges", z, 'Visible', 'off').Values;
         CpastNorm = h_past/dz*N/nPart;
         CpresentNorm = h_present/dz*N/nPart;
-        C_hist(step,:) = CpresentNorm;
-        
-        MSEh(step,1) = MSE(CpresentNorm,test);
-        
+
                 
         dC = max(abs(CpresentNorm - CpastNorm)/dt);
         
@@ -83,7 +82,7 @@ while OnContinue
         if mod(t,60)<dt/2
             disp([' Temps : ' num2str(t/tf*100) '% - Ecart : ' num2str(dC)])
         end
-
+        
         z_past = z_present;
     end
 end
