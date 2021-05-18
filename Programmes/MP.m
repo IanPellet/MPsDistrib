9ccbb21ccfe4658ca9b5d0ee9ffb4c04b7f3f213
Ian <pellet.ian1@gmail.com>
2021-05-18 18:34:57 +0200

    mp transport time complexity checked

--- Programmes/MP.m
+++ Programmes/MP.m
@@ -1,26 +1,16 @@
 classdef MP
     %MP Micro-plastic class, represents one particle
-    %   Detailed explanation goes here
-    
-    properties     
-        z_ % particle's position (m)
-
-        dz_
+    %  
+    properties
+        rhow_
     end
-    
     properties (SetAccess = private)
         type_ % int {0,1,2,3} NON IMPLÉMENTÉ
         size_ % (m)
         rho_ % (kg.m⁻³)
         U_ % velocity in the water column, dep of type (m.s⁻¹)
     end
-    
-    properties (Dependent)
-        
-        uz_
-        index_
-    end
-    
+
     methods
         function obj = MP(size, rhop, rhow)
             %MP Constructor
@@ -30,7 +20,6 @@ classdef MP
             % PARAMETERS
             % size : double, particle size (m)
             % rhop : double, particle density (kg.m⁻³)
-            % z : double, particle's position (m)
             % rhow : double, water density ((kg.m⁻³)
             %
             
@@ -39,84 +28,30 @@ classdef MP
             if nargin == 0 % default constructor
                 obj.size_ = 0;
                 obj.rho_ = 0;
-%                 obj.z_ = 0;
-%                 obj.dz_ = 0;
-                obj.U_ = 0;
+                obj.rhow_ = 0;
             else
                 obj.size_ = size;
                 obj.rho_ = rhop;
-%                 obj.z_ = z;
-%                 obj.dz_ = dz;
-                obj.U_ = obj.setU(rhow);
-            end
-            
-            
-            
+                obj.rhow_ = rhow;
+            end           
         end
         
-
-        function value = setU(obj,rhow)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
+        function value = get.U_(obj)
+            %SETU Compute fall velocity of particle in the water column
+            %   
             Nom=[... 
                 ;{'Nielsen'}...%Nielsen (1992)
                 ;{'Soulsby'}...%Soulsby (1997)
                 ;{'Ahrens'}...%Ahrens (2000)
                ];
             indNom = 3;
-            S = obj.rho_./rhow; 
+            S = obj.rho_./obj.rhow_; 
             Ws = eval(['Vitesse' cell2mat(Nom(indNom)) '(obj.size_,S);']);
             value = Ws'; 
-            test = obj.rho_<rhow;
+            test = obj.rho_<obj.rhow_;
             value(test) = -Ws(test);
         end
-        
-%         function uzVal = get.uz_(obj)
-%             uzVal = obj.U_(obj.index_);
-%         end
-%         
-%         function iVal = get.index_(obj)
-%             iVal = max(1, cast(obj.z_/obj.dz_, 'uint32'));
-%         end
-%         
-%         function new_z = Step_Lagrangien(obj, K, dK, dt, L)
-%             %STEP_LAGRANGIEN One step foward in time for the Lagrangian transport model
-%             %   The new value of x after dt is computed and returned by this function.
-%             
-%             Kz = K([obj.index_]);
-%             dKz = dK([obj.index_]);
-%             
-%             pd = makedist('Uniform', 'lower', -1, 'upper', 1);
-%             d = 1/3;
-%             R = random(pd,size(obj));
-% 
-%             new_z = obj.z_ + dt*obj.uz_ + dKz*dt + R.*(2/d*Kz*dt).^(1/2);
-%             % First-type boundary conditions
-%             % new_x = max(0,new_x);
-%             % new_x = min(L, new_x);
-% 
-%             % Reflective boundary conditions
-%             new_z(new_z < 0) = -new_z(new_z < 0);
-%             new_z(new_z > L) = L-new_z(new_z > L) + L;
-% 
-%             end
-        
-        %         function rho = get.rho_(obj)
-%             %METHOD1 Summary of this method goes here
-%             %   Detailed explanation goes here
-% %             load('rho_per_type', 'RHO_TYPE'); % loads a variable RHO_TYPE 
-%             RHO_TYPE = [ 900 1050 ;
-%                         1000 1300 ;
-%                         1300 1400 ];
-%             % RHO_TYPE = [rho_min1 rho_max1 ;
-%             %             rho_min2 rho_max2 ;
-%             %             rho_min3 rho_max3 ]
-%             m = RHO_TYPE(obj.type_,1); % min Rho
-%             M = RHO_TYPE(obj.type_,2); % max Rho
-%             pd = makedist('Normal','mu',(m+M)/2 ,'sigma', (M-m)/4 ); % Normal law, centered on (m+M)/2, 95.45% of values between m and M
-%             rho = random(pd);
-%         end
-        
+
     end
 end
 
--- Programmes/MP_transport.m
+++ Programmes/MP_transport.m
@@ -2,7 +2,8 @@
 
 clear
 
-
+tf = 60*60*24*2; % Simulation time
+dt_test = 60*60;
 
 %% Water column parameters
 L = 50;
@@ -24,8 +25,9 @@ rhow = interp1(-z__day,Row_day,z,'pchip'); % density of sea water
 %% Particules initialisation
 nPart = 5e3; % number of particles
 zPart = linspace(0, L, nPart); % depth of particles
-sizePart = linspace(250e-6, 5e-3, nPart); % size of particle
-rhop = 1000;
+sizePart = linspace(250e-6, 500e-6, nPart); % size of particle
+% sizePart = ones(size(zPart))*400e-6;
+rhop = 1020;
 
 % allocate memory to store particles
 clear part
@@ -35,35 +37,52 @@ for i = 1:(nPart)
     mp(i) = MP(sizePart(i), rhop, rhow);
 end
 
+U = [mp.U_];
+
+%% Time step init
+dt = 10;
+ddK = diff(dK)/dz;
+dt = min(dt, abs(min(1./ddK)/10)); % condition dt<<min(1/ddK) 
+dt = min(dt, dz/max(max(abs(U)))); % condition dt < dz/max|u|
+
+%% Initial conditions
+h_init = histogram(zPart, "BinEdges", z, 'Visible', 'off').Values;
+CiNorm = h_init/dz*N/nPart;
+
 %% Simulation
-dt = 10; % Time step
-tf = 60*60; % Simulation time
-nStep = tf/dt+1; % Number of steps
+nStep = tf/dt_test+1;
 if cast(nStep, 'uint32') ~= nStep
     nStep = cast(nStep+1, 'uint32');
 end
-T = ones(nStep,1); % allocate memory to store time
+T = ones(nStep,1);
+ChNorm = ones(nStep,N);
 
 z_past = zPart;
 step = 1;
+ChNorm(step,:) = [CiNorm];
 
 t=0; OnContinue=true;
 T(step,1) = t;
+% uz = zeros(size(zPart));
 while OnContinue
-    step = step+1;
     % Time update
     t=t+dt;
-    T(step,1) = t;
     
     %% Particules update
     index = max(1, cast(zPart/dz, 'uint32'));
-    U = [mp.U_];
-    uz = diag(U(index,:))';
+
+    for i=1:length(zPart)
+        uz = U(index(i),i);
+    end
+    
+    
     zPart = Step_Lagrangien(zPart, uz, K(index), dK(index), dt, L);
     
-    
-    if true
 
+    if (mod(t,dt_test)<=dt/2 || dt_test-mod(t,dt_test)<=dt/2 ) 
+    %if true
+        step = step+1;
+        T(step,1) = t;
         z_present = zPart;
         
         % Computation of the concentration of MPs in each mesh
@@ -71,7 +90,7 @@ while OnContinue
         h_present = histogram(z_present, "BinEdges", z, 'Visible', 'off').Values;
         CpastNorm = h_past/dz*N/nPart;
         CpresentNorm = h_present/dz*N/nPart;
-
+        ChNorm(step,:) = CpresentNorm;     
                 
         dC = max(abs(CpresentNorm - CpastNorm)/dt);
         
@@ -80,10 +99,13 @@ while OnContinue
         end
         
         if mod(t,60)<dt/2
-            disp([' Temps : ' num2str(t/tf*100) '% - Ecart : ' num2str(dC)])
+            disp([' Temps : ' num2str(t/tf*100) '% - Ecart : ' num2str(dC/sum(CpresentNorm)*100), '%'])
         end
         
         z_past = z_present;
     end
+
 end
+CfinalNorm = CpresentNorm;
+plot(CfinalNorm, -z_)
 
