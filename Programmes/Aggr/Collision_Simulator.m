a30a2bdf4f30774fb4a4fed05ee097349b55d0b4
Ian <pellet.ian1@gmail.com>
2021-07-26 14:29:25 +0200

    fixed comparison real collision vs computed collision probability

--- Programmes/Aggr/Collision_Simulator.m
+++ Programmes/Aggr/Collision_Simulator.m
@@ -1,9 +1,9 @@
-function [collision, dt] = Collision_Simulator(mpList, aggList, mpZinit, aggZinit, K, dK, L, dz, tf, dt_test, dtTheo)
+function [collision, dt] = Collision_Simulator(mpList, aggList, mpZinit, aggZinit, K, dK, L, dz, tf, dt_test, dt_coll)
 
 
 fprintf(['\n\n--------------------- Simulation running ---------------------\n'])
 
-    figure(5); clf,
+    figure(1); clf,
 
     mpU = [mpList.Ws]; % MP fall velocities on the column (m.s⁻¹) 
     aggU = [aggList.Ws]; % OrgaAggr fall velocities on the column (m.s⁻¹) 
@@ -17,12 +17,20 @@ fprintf(['\n\n--------------------- Simulation running ---------------------\n']
 
     sizePart = [mpList.Size aggList.Size];
     nPart = length(sizePart);
-
-    SumD = (reshape([mpList.Size], 1, length(mpList)) + reshape([aggList.Size], length(aggList), 1));
+    
+    mpSize = reshape([mpList.Size], 1, length(mpList));
+    aggSize = reshape([aggList.Size], length(aggList), 1);
+    
+    mpFree = [mpList.Locked]==0;
+    mpIndex = 1:length(mpList);
     
     col = [repmat([0 0.4470 0.7410],length(mpZ),1) ; repmat([0.4660 0.6740 0.1880],length(aggZ),1)];
     
     collision = cell(0);
+    
+    
+    d = 1;
+    StepPD = makedist('Normal', 'mu', 0, 'sigma', d);
     
     %% Time step initialisation
 
@@ -53,11 +61,31 @@ fprintf(['\n\n--------------------- Simulation running ---------------------\n']
     dt = min(dt, abs(min(1./ddK)/10)); % check condition dt<<min(1/ddK) 
     dt = min(dt, dz/max(max(abs([mpU aggU])))); % check condition dt < dz/max|u|
     
-    clear ddK meanDU,
+    clear ddK meanDU B D,
+    
+    %% INIT collisions
+    boom = 0; % number of collisions 
+%     X = round(dt_coll/dt);
+%     sauveXdt = cell(X,2);
+%     t_Xdti = -X; % Index at which we can find the positions at t-Xdt
+%     iSauve = 0; % Index to at which to save curent values
 
     %% Simulation
-    t=0; 
-    boom = 0; % number of collisions 
+    t=0;
+    X = 0;
+    if t+dt/2 >= X*dt_coll
+%         disp('POUET')
+        X = X+1;
+        mpt_Xdt = mpZ;
+        aggt_Xdt = aggZ;
+    end
+    
+%     t_Xdti = (t_Xdti>0)*(mod(t_Xdti, X)+1) + (t_Xdti<=0)*(t_Xdti+1);
+%     iSauve = mod(iSauve, X) +1;
+%     sauveXdt{iSauve,1} = mpZ;
+%     sauveXdt{iSauve,2} = aggZ;
+           
+    
     OnContinue=true;
 %       %% Plot
 % 
@@ -77,6 +105,10 @@ fprintf(['\n\n--------------------- Simulation running ---------------------\n']
         % Time update
         t=t+dt;
         
+%         t_Xdti = (t_Xdti>0)*(mod(t_Xdti, X)+1) + (t_Xdti<=0)*(t_Xdti+1);
+%         iSauve = mod(iSauve, X) +1;
+    
+        
         %% Particules update
         mpI= cast(max(1, fix(mpZ/dz)), 'uint8'); % int array, index of each particle's current mesh
         aggI= cast(max(1, fix(aggZ/dz)), 'uint8'); % int array, index of each particle's current mesh
@@ -89,42 +121,53 @@ fprintf(['\n\n--------------------- Simulation running ---------------------\n']
             aggUz(i) = aggU(aggI(i),i);
         end
         
-        mpZn_1 = mpZ;
-        aggZn_1 = aggZ;
-        % Update position 
-        mpZ = Step_Lagrangien(mpZ, mpUz, K(mpI), dK(mpI), dt, L);
-        aggZ = Step_Lagrangien(aggZ, aggUz, K(aggI), dK(aggI), dt, L);
+        % Update position of aggregates and free MPs 
+        mpZ(mpFree) = Step_Lagrangien_noDist(mpZ(mpFree), mpUz(mpFree), K(mpI(mpFree)), dK(mpI(mpFree)), dt, L, StepPD);
+        aggZ = Step_Lagrangien_noDist(aggZ, aggUz, K(aggI), dK(aggI), dt, L, StepPD);
         
+        % Update position of locked MPs (same position as the aggregate
+        % it's locked on)
+        mpZ(~mpFree) = aggZ([mpList(~mpFree).Locked]);
+        
+        % Save position
+%         sauveXdt{iSauve,1} = mpZ;
+%         sauveXdt{iSauve,2} = aggZ;
+        if t+dt/2 >= X*dt_coll
+%             disp('POUET')
+            X = X+1;
+            mpt_Xdt = mpZ;
+            aggt_Xdt = aggZ;
+        end
+
         %% Collision
-        mpPos = reshape(mpZ, 1, length(mpZ));
+        mpPos = reshape(mpZ(mpFree), 1, length(mpZ(mpFree)));
         aggPos = reshape(aggZ, length(aggZ), 1);
         DeltaPos = abs(mpPos - aggPos);
       
+        SumD = (mpSize(mpFree) + aggSize);
         
         [aggClose, mpClose] = find(DeltaPos <= SumD./2);
-         for iClose = 1:length(aggClose)
-%              disp('POUET')
-            iagg = aggClose(iClose);
-            imp = mpClose(iClose);
+        
+        mpIndexFree = mpIndex(mpFree);
+        
+%         if ~ isempty(aggClose) && t_Xdti>0
+        if ~ isempty(aggClose)
+            % get mp and agg positions at t-Xdt
+%             mpt_Xdt = sauveXdt{t_Xdti,1};
+%             aggt_Xdt = sauveXdt{t_Xdti,2};
             
-            boom = boom +1;
-            
-            mpI= cast(max(1, fix(mpZn_1(imp)/dz)), 'uint8'); % int array, index of MP's mesh at t-dt
-            aggI= cast(max(1, fix(aggZn_1(iagg)/dz)), 'uint8'); % int array, index of Agg's mesh at t-dt
-            % Find fall velocity of each particle
-            mpUz2 = mpU(mpI, imp);
-            aggUz2 = aggU(aggI, iagg);
-            
-            delU = mpUz2 - aggUz2;
-            deldK = dK(mpI)-dK(aggI)';
-            sumSqrtK = sqrt(K(mpI)) + sqrt(K(aggI)');
-            delMax = abs(delU + deldK).*dt + sumSqrtK.*sqrt(6*dt);
-            
-            collision{boom} = [mpZn_1(imp)-aggZn_1(iagg) delMax (mpList(imp).Size+aggList(iagg).Size)/2];
-            
+            for iClose = 1:length(aggClose)
+                iagg = aggClose(iClose);
+                imp = mpIndexFree(mpClose(iClose));
+                
+                boom = boom +1;
+
+                collision{boom} = [mpt_Xdt(imp)-aggt_Xdt(iagg) (mpList(imp).Size+aggList(iagg).Size)/2];
+                
+                [aggList(iagg), mpList(imp)] = aggList(iagg).aggrMP(mpList(imp));
+                mpFree(imp) = false; 
+            end
         end
-
-
         
 
 %         %% Plot 
@@ -139,7 +182,25 @@ fprintf(['\n\n--------------------- Simulation running ---------------------\n']
 % %         title(['t = ', num2str(hh), ':', num2str(mm)])
 %         title(['t = ', num2str(t)])
 %         pause(0)
-
+%  %% Plot
+%         zPartPlot = [mpZ aggZ];
+%         
+%         if sum(~mpFree)~=0
+%             col(~mpFree,:) = repmat([0.8500 0.3250 0.0980], sum(~mpFree),1);
+%             col(length(mpZ) + [mpList(~mpFree).Locked], :) = repmat([0.4940 0.1840 0.5560], sum(~mpFree),1);
+%         end
+%         
+%         
+%         scatter(1:length(zPartPlot), -zPartPlot, sizePart*1e5/2, col,'filled')
+%         xlim([1 nPart])
+%         ylim([-31 -30])
+%         xlabel("Particles")
+%         ylabel("Depth (m)")
+% %         hh = fix(t/60/60);
+% %         mm = fix(t/60-hh*60);
+% %         title(['t = ', num2str(hh), ':', num2str(mm)])
+%         title(['t = ', num2str(t)])
+%         pause(0)
     
         % Test
         if (mod(t,dt_test)<=dt/2 || dt_test-mod(t,dt_test)<=dt/2 )
@@ -154,6 +215,26 @@ fprintf(['\n\n--------------------- Simulation running ---------------------\n']
         end
 
     end
+    
+     %% Plot
+        zPartPlot = [mpZ aggZ];
+        
+        if sum(~mpFree)~=0
+            col(~mpFree,:) = repmat([0.8500 0.3250 0.0980], sum(~mpFree),1);
+            col(length(mpZ) + [mpList(~mpFree).Locked], :) = repmat([0.4940 0.1840 0.5560], sum(~mpFree),1);
+        end
+        
+        
+        scatter(1:length(zPartPlot), -zPartPlot, sizePart*1e5/2, col,'filled')
+        xlim([1 nPart])
+        ylim([-31 -30])
+        xlabel("Particles")
+        ylabel("Depth (m)")
+%         hh = fix(t/60/60);
+%         mm = fix(t/60-hh*60);
+%         title(['t = ', num2str(hh), ':', num2str(mm)])
+        title(['t = ', num2str(t)])
+        pause(0)
     
 
 end
